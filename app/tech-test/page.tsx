67cfa25719385b163aae5f501d87ed297ef541a9
sooyshim <sooyshim@gmail.com>
2023-06-24 20:56:07 -0400

    boilder plate

--- app/tech-test/page.tsx
+++ app/tech-test/page.tsx
@@ -1,17 +1,17 @@
 import { FC } from "react";
 
-//1. Fetch data from the url `https://test.marvel/v1/public/characters/:id`
-
-// const characters = [
-//     { id: "1", name: "Iron man", date: "2023" },
-//     { id: "2", name: "Spider man", date: "2023" },
-//     { id: "3", name: "Black Panther", date: "2023" },
-//     { id: "4", name: "Captain America", date: "2023" },
-// ];
-// 2. Find Iron man from the array
-// 3. Create an array containing only names
-
 const Test: FC = () => {
+    //1. Fetch data from the url `https://test.marvel/v1/public/characters/:id`
+
+    // const characters = [
+    //     { id: "1", name: "Iron man", date: "2023" },
+    //     { id: "2", name: "Spider man", date: "2023" },
+    //     { id: "3", name: "Black Panther", date: "2023" },
+    //     { id: "4", name: "Captain America", date: "2023" },
+    // ];
+    // 2. Find Iron man from the array
+    // 3. Create an array containing only names
+
     return (
         <div className="ml-6 mt-6 flex flex-col">
             <h1 className="text-2xl ">Level Test</h1>
@@ -20,6 +20,7 @@ const Test: FC = () => {
                 <li className="mt-3"> - Tell me about yourself.</li>
                 <li> - What is a virtual DOM?</li>
                 <li> - Why would you use React.js?</li>
+                <li> - What are the benefits of using TDD?</li>
             </ul>
             <h2 className="mt-10 text-lg font-semibold">Tect Challenge</h2>
             <p className="mt-3">Go back to repo</p>
